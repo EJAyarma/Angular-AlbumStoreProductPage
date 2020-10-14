98ebaf76946c653d8eabe8b33ddb5229e882282b
Emmanuel John Ayarma <johnayarmahe@gmail.com>
2020-10-14 14:01:24 +0000

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
 import 'rxjs/add/operator/map';
+import { Album } from './album';
 
 
 @Injectable({
@@ -11,9 +14,9 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id:number) {
+  Observable<Album> getAlbum(id:number) {
     return this._http.get(this._albumUrl).map( (response) => {
-      response.json();
+      <Album> response.json();
     });
   }
 }
