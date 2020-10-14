3e194839bfd29110e5b9d4ecf0599ae50d30a9b2
Emmanuel John Ayarma <johnayarmahe@gmail.com>
2020-10-14 12:50:27 +0000

    started module 3

---
+++ src/app/product.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private _albumUrl:string = '../assets/album.json';
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id:number) {
+    return this._http.get(_albumUrl).map(Response.json());
+  }
+}
